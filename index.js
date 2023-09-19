8e869dc28d28b1b83f4149c1c0c250de0c252408
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-09-18 22:21:23 -0600

    added render and file making function, and bananas

---
+++ index.js
@@ -0,0 +1,83 @@
+const inquirer = require("inquirer");
+
+const fs = require("fs");
+
+const { Circle, Square, Triangle, SVG } = require("./lib/shapes");
+
+const questions = [
+  {
+    name: "shape",
+    type: "list",
+    message: "Choose a shape?",
+    choices: ["circle", "triangle", "square"],
+  },
+  {
+    name: "shapeColor",
+    type: "list",
+    message: "What color would you like your shape?",
+    choices: [
+      "black",
+      "red",
+      "blue",
+      "pink",
+      "green",
+      "purple",
+      "yellow",
+      "orange",
+      "white",
+    ],
+  },
+  {
+    name: "text",
+    type: "input",
+    message: "What three letters would you like for your logo?",
+  },
+  {
+    name: "textColor",
+    type: "list",
+    message: "What color would you like your text?",
+    choices: [
+      "black",
+      "red",
+      "blue",
+      "pink",
+      "green",
+      "purple",
+      "yellow",
+      "orange",
+      "white",
+    ],
+  },
+];
+
+function bananas() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+
+    const { shape, shapeColor, text, textColor } = answers;
+
+    let userShape;
+
+    switch (shape) {
+      case "circle":
+        userShape = new Circle(shapeColor);
+        break;
+      case "square":
+        userShape = new Square(shapeColor);
+        break;
+      case "triangle":
+        userShape = new Triangle(shapeColor);
+        break;
+    }
+
+    const svg = new SVG(text, textColor, userShape);
+
+    const mySVG = svg.render();
+
+    fs.writeFile("newLogo.svg", mySVG, (err) =>
+      err ? console.log("🚫🍌") : console.log("🍌🍌 New Logo made nice job 🍌🍌")
+    );
+  });
+}
+
+bananas();
